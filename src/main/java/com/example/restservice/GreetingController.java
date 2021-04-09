fb45c62e271c4ab8bc90d2a08d544335066d43a5
therss99here <rahulsinghsengar009@gmail.com>
2021-04-09 13:22:36 +0530

    moved to restservice branch

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "value", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String .format(template, name));
+    }
+
+    @GetMapping("/greeting/{value}")
+    public String customGreeting(@PathVariable(value = "value")String value){
+        return "Hello " + value + " !!";
+    }
+}
